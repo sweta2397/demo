5c06adaee6392e3730de6feef326f57fef05cd6c
swetadj <102693276+swetadj@users.noreply.github.com>
2022-04-01 14:06:52 -0400

    Add files via upload

---
+++ 01_dic_syntax.py
@@ -0,0 +1,11 @@
+myDict = {
+    "Fast": " In a Quick manner",
+    "Harry": "A coder",
+    "Marks":[1,2,3],
+    "anotherdict": {'harry': 'Player'}
+}
+# print(myDict['Fast'])
+# print(myDict['Harry'])
+myDict['Marks'] = [45,78]
+print(myDict['marks'])
+print(myDict['anotherdict']['harry'])
